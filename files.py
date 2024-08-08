eafad4150b04c06d2f3bc12fe0530ef125048bcc
Yuto Urashima <yuto.urashima24@gmail.com>
2024-08-08 17:59:23 +0900

    refactor: 処理をファイルごとに分割

---
+++ files.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
---
+++ models.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+# 機械学習モデルの名前
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    letnet = "letnet"
+
+
+app = FastAPI()
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "letnet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
