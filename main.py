5a059aa50e4c831cce8cfd80f20f7c8325072e62
Yuto Urashima <yuto.urashima24@gmail.com>
2024-08-08 17:54:45 +0900

    feat: パスパラメータ

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+from enum import Enum
+
 from fastapi import FastAPI
+
+
+# 機械学習モデルの名前
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    letnet = "letnet"
+
 
 app = FastAPI()
 
@@ -6,3 +16,24 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "letnet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
