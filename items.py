4b2a074ad0cf55daf4874801f4807555757f59ec
Yuto Urashima <yuto.urashima24@gmail.com>
2024-08-08 21:45:47 +0900

    feat: リクエストボディ

--- items.py
+++ items.py
@@ -1,6 +1,15 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
 
 app = FastAPI()
 
@@ -31,3 +40,22 @@ async def read_user_item(
 ):
     item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
     return item
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return {"item_id": item_id, **item.dict()}
